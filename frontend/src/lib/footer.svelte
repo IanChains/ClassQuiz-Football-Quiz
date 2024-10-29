319d5766e97d604345877c0aea8355080a5bfac8
IanChains <49492286+IanChains@users.noreply.github.com>
2024-10-29 22:44:43 +0100

    Edit Footer
    small change to footer

--- frontend/src/lib/footer.svelte
+++ frontend/src/lib/footer.svelte
@@ -10,12 +10,16 @@ SPDX-License-Identifier: MPL-2.0
 			Created by the open source project: <a href="https://classquiz.de/" target="_blank" class="underline text-blue-300 hover:text-blue-500 transition">Classquiz</a>!
 			<br>
 			With special thanks to <a href="https://mawoka.eu" target="_blank" class="underline text-blue-300 hover:text-blue-500 transition">Mawoka</a> and <a href="https://classquiz.de/docs/attribution/" target="_blank" class="underline text-blue-300 hover:text-blue-500 transition">his team</a>!
+			<br>
 			Do you love this? Please consider <a href="https://mawoka.eu/donate" target="_blank" class="underline text-blue-300 hover:text-blue-500 transition">donating</a> for this open source project.
 			<br>
 			<br>
 			Adapted with LOVE ♥ by <a href="https://ian-chains.it" target="_blank" class="underline text-blue-300 hover:text-blue-500 transition">Ian-Chains IT</a>!
 			<br>
 			Your personalized IT guidance, just for you - always by your side!
+			<br>
+			<br>
+			You can find the source code in this <a href="https://github.com/IanChains/ClassQuiz-Football-Quiz" target="_blank" class="underline text-blue-300 hover:text-blue-500 transition">GitHub Repository</a>!
 		</p>
 	</div>
 </footer>
